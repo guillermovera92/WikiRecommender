d132107fc4e7a9a738f289ee88beb0f00afa04fb
Sam Olsen <shorlbec@macalester.edu>
2015-12-02 19:42:23 -0600

    Add doc count function

--- recommender.py
+++ recommender.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python
 import re
+from collections import defaultdict
 
 def create_cleaned_set(articles):
     out = []
     for article in articles:
-        obj = re.match(r'.*is a[^\\.]*(singer|songwriter|album|song|record|musician|performer).*', article, re.I)
+        obj = re.match(r'.*is a[^\\.]*(singer|songwriter|album|song|record|musician|performer).*', article.body, re.I)
         if obj != None:
             out.append(article)
     return out
+
+def calc_doc_counts(docs):
+    def split(s):
+        return re.split('\W+', s.lower())
+        
+    doc_count = defaultdict(int)
+    for d in docs:
+        seen = set()
+        words = split(d.body)
+        for word in words:
+            if word in seen:
+                continue
+            seen.add(word)
+            doc_count[word] += 1
+    return doc_count
+
+def generate_frequency_vector(a, b, blacklist):
 
 all_articles = [] # This will hook into Guillermo's code
 
